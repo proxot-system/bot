c1ec17ab0338e3578ec4362bb58925c21af87b4f
meqativ <meqativ@users.noreply.github.com>
2026-03-28 19:20:46 +0200

    feat(roll): increase default limits

--- src/extensions/commands/roll.py
+++ src/extensions/commands/roll.py
@@ -22,8 +22,8 @@ class RollCommand(Extension):
 	@slash_command(description="Roll an imaginary dice")
 	@slash_option(
 		description="What sided dice to roll",
-		min_value=1,
-		max_value=9999,
+		min_value=0,
+		max_value=float("9" * 15),
 		name="sides",
 		opt_type=OptionType.INTEGER,
 		required=True,
@@ -31,7 +31,7 @@ class RollCommand(Extension):
 	@slash_option(
 		description="How many times to roll it",
 		min_value=1,
-		max_value=10,
+		max_value=234,  # todo: dynamically calculate this from dice side amount (and get it from config)
 		name="amount",
 		opt_type=OptionType.INTEGER,
 	)
@@ -45,7 +45,7 @@ class RollCommand(Extension):
 	async def roll(self, ctx: SlashContext, sides: int, amount: int = 1, public: bool = False):
 		loc = Localization(ctx, prefix="commands.roll")
 
-		rolls = [random.randint(1, sides) for _ in range(amount)]
+		rolls = [0 if sides == 0 else random.randint(1, sides) for _ in range(amount)]
 
 		result = amperjoin([str(roll) for roll in rolls])
 		description = await locale_format(loc, loc.get_string("desc"), result=result)
