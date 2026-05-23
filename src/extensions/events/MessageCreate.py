eb20d6a2f93451298c403ad224ec18912a705183
meqativ <meqativ@users.noreply.github.com>
2026-05-23 17:08:46 +0300

    feat(loc_commands): delete

--- src/extensions/events/MessageCreate.py
+++ src/extensions/events/MessageCreate.py
@@ -2,13 +2,10 @@ from interactions import Extension, listen
 from interactions.api.events import MessageCreate
 
 from utilities.dev_commands import execute_dev_command
-from utilities.loc_commands import execute_loc_command
 
 
 class MessageCreateEvent(Extension):
 	@listen(MessageCreate)
 	async def handler(self, event: MessageCreate):
 		client = event.client
-		res = await execute_dev_command(event.message)
-		if not res:
-			await execute_loc_command(event.message)
+		await execute_dev_command(event.message)
