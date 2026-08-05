59b18b8a7ba1214c2c1c519cdc6256af139bbf02
meqativ <meqativ@users.noreply.github.com>
2026-08-05 19:24:44 +0300

    wawa

--- src/extensions/events/MessageCreate.py
+++ src/extensions/events/MessageCreate.py
@@ -7,4 +7,6 @@ from utilities.dev_commands import execute_dev_command
 class MessageCreateEvent(Extension):
 	@listen(MessageCreate)
 	async def handler(self, event: MessageCreate):
+		if "flip" in event.message.content and event.message.contains_mention(event.client.user):
+			await event.message.reply(file="src/data/images/other/backflip.gif")  # pyright: ignore[reportArgumentType]
 		await execute_dev_command(event.message)
