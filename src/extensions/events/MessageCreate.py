1685f62b5bb4c10dd1252b2acb8e4476918c8afe
meqativ <meqativ@users.noreply.github.com>
2026-08-05 19:32:51 +0300

    make backflip ww[p

--- src/extensions/events/MessageCreate.py
+++ src/extensions/events/MessageCreate.py
@@ -8,5 +8,7 @@ class MessageCreateEvent(Extension):
 	@listen(MessageCreate)
 	async def handler(self, event: MessageCreate):
 		if "flip" in event.message.content and event.message.contains_mention(event.client.user):
-			await event.message.reply(file="src/data/images/other/backflip.gif")  # pyright: ignore[reportArgumentType]
+			await event.message.reply(
+				file="https://cdn.discordapp.com/attachments/1336864890706595852/1534599945448067112/backflip.webp?ex=6a74b712&is=6a736592&hm=8393c2251e5ee8054371991bcdb506fcca9621c98e54accc677da9e3eb040155&"  # pyright: ignore[reportArgumentType]
+			)
 		await execute_dev_command(event.message)
